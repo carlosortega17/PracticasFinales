f54008dd0d7ce04330d22f6d38fbb2012608a334
= <=>
2021-12-06 15:02:39 -0800

    Ejercicio 2.1

---
+++ 3.3_Practica/ejercicio2_1.s
@@ -0,0 +1,10 @@
+@ Ejercicio: 2.1
+@ Alumno: Ortega Frias Carlos Eduardo | #17210612
+@ Escribe un bucle for que imprima los 50 primeros
+@ numeros pares naturales en orden inverso (desde 100 
+@ hasta 2 en pasos de 2).
+@ Una vez hecho esto, aplica desenrollamiento de 
+@ bucle de tal forma que el salto
+@ condicional se ejecute 10 veces, con 5 repeticiones cada vez.
+.data
+.text

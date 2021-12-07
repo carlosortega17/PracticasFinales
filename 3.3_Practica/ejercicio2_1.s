9ed7f2174f94697d81c5c3afbbe8b5b50eef549a
Carlos Eduardo Ortega Frias <carlos.ortega17@tectijuana.edu.mx>
2021-12-06 16:38:54 -0800

    Ejercicio2.1

--- 3.3_Practica/ejercicio2_1.s
+++ 3.3_Practica/ejercicio2_1.s
@@ -1,10 +1,21 @@
-@ Ejercicio: 2.1
-@ Alumno: Ortega Frias Carlos Eduardo | #17210612
-@ Escribe un bucle for que imprima los 50 primeros
-@ numeros pares naturales en orden inverso (desde 100 
-@ hasta 2 en pasos de 2).
-@ Una vez hecho esto, aplica desenrollamiento de 
-@ bucle de tal forma que el salto
-@ condicional se ejecute 10 veces, con 5 repeticiones cada vez.
+@ Ortega Frias Carlos Eduardo | #17210612
 .data
-.text
+formato: .asciz "%d\012" @ Formato para mostrar el numero
+.text @ Etiqueta de texto
+.global	main @ Funcion principal
+main: @ Funcion Main
+	push	{r4, lr} @ Agregamos al registro r4
+	mov	r5, #90 @ Asignamos el valor a r5 con 90
+.ciclo1: @ Primer ciclo, For de aumento en 10
+	add	r4, r5, #10
+.ciclo2:
+	mov	r1, r4 @ Movemos al registro r1
+	ldr	r0, =formato @ Asignamos el valor de Formato
+	sub	r4, r4, #2 @ Aumentamos el registro r4 en 2
+	bl	printf @ Ejecutamos la funcion printf para mostrar en pantalla
+	cmp	r5, r4 @ Comparamos el numero de r5 con r4
+	bne	.ciclo2 @ Repetimos el ciclo 2
+	sub	r5, r5, #10 @ Eliminamos al registro r5 en 10
+	cmn	r5, #10 @ Verificamos el minimo de r5
+	bne	.ciclo1 @ Repetimos el ciclo 1
+	pop	{r4, pc} @ Eliminamos de r4

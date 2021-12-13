526a926f06d9ba5fcd553bac33a1ffd024830bb3
Carlos Eduardo Ortega Frias <carlos.ortega17@tectijuana.edu.mx>
2021-12-12 22:23:56 -0800

    3.5

---
+++ 3.5_Practica/ejercicio1.s
@@ -0,0 +1,10 @@
+	.set GPBASE, 0x20200000
+	.set GPFSEL0, 0x00
+	.set GPSET0, 0x1c
+.text
+	ldr r0, =GPBASE
+	mov r1, #0b00001000000000000000000000000000
+	str r1, [r0, #GPFSEL0]
+	mov r1, # 0b00000000000000000000001000000000
+	str r1, [r0, #GPSET0]
+infi:	b	infi
